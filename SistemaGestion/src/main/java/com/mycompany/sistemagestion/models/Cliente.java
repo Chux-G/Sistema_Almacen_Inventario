1a945cffa8a39b97bcd87f60bf2892cceee63085
Chux-G <chsilvar49@gmail.com>
2022-06-06 16:39:46 -0400

    SistemaGestion

---
+++ SistemaGestion/src/main/java/com/mycompany/sistemagestion/models/Cliente.java
@@ -0,0 +1,47 @@
+package com.mycompany.sistemagestion.models;
+
+
+public class Cliente {
+    
+    public String nombre;
+    public String apellido;
+    public String email;
+    public String telefono;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+    
+    public String getNombreCompleto(){
+        return nombre+" "+apellido;
+    }
+    
+}
